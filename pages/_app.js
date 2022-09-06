88cbc7ed3d2fd17b0cdd659a2e8a2bbe3d5d2c42
Lucy Toman <lucy@toman.me.uk>
2022-09-06 13:42:46 +0200

    adds next-auth and links up local dB

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "/styles/globals.css"
 import Header from "/components/Header"
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }) {
     return (
-        <>
+        <SessionProvider session={pageProps.session}>
             <Header />
             <Component {...pageProps} />
-        </>
+        </SessionProvider>
     )
 }
 
