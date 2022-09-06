3e41761fefbb14b492923e2fd38f9e53a608dbe5
Lucy Toman <lucy@toman.me.uk>
2022-09-06 13:05:08 +0200

    initial set up

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Header() {
+    return (
+        <>
+            <Head>
+                <title>Project Manager</title>
+                <meta name="description" content="project manager" />
+                <link rel="icon" href="/bookmark.ico" />
+            </Head>
+            <header> Project Manager Header</header>
+        </>
+    )
+}
