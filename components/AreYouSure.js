05086a45ab4184d509e4854bb8e2ee79cf4b8edc
Lucy Toman <lucy@toman.me.uk>
2022-09-12 12:44:06 +0200

    minor style tweak

--- components/AreYouSure.js
+++ components/AreYouSure.js
@@ -4,7 +4,7 @@ export default function AreYouSure({ project, setSureDisplay }) {
     const router = useRouter()
 
     return (
-        <div className="flex flex-row text-red-700">
+        <div className="flex flex-row text-red-700 mt-1">
             <h1>Delete entire project?</h1>
             <button
                 className="cursor-pointer ml-4 px-2 border border-red-700 hover:bg-emerald-700 hover:border-emerald-900 hover:text-yellow-50"
--- components/AreYouSureTask.js
+++ components/AreYouSureTask.js
@@ -4,7 +4,7 @@ export default function AreYouSureTask({ todo, setSureDisplay }) {
     const router = useRouter()
 
     return (
-        <div className="flex flex-row text-red-700">
+        <div className="flex flex-row text-red-700 mt-1">
             <h3>Delete this task?</h3>
             <button
                 className="cursor-pointer ml-4 px-2 border border-red-700 hover:bg-emerald-700 hover:border-emerald-900 hover:text-yellow-50"
